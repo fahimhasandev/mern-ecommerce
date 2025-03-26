4b4b91c886215236d0a893c29f443199ae673cc7
fahimhasandev <fhasan5@wgu.edu>
2025-03-26 14:35:19 -0500

    Preparing server

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomeScreen from '../screens/HomeScreen';
 import Main from '../layout/main';
+import ProductScreen from '../screens/ProductScreen';
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: '/',
         Component: HomeScreen,
+      },
+      {
+        path: '/product/:id',
+        Component: ProductScreen,
       },
     ],
   },
