fcd2d1309d9b5457357324a53934203a783d4d93
fahimhasandev <fhasan5@wgu.edu>
2025-04-01 22:40:38 -0500

    created redux toolkit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,13 @@ import './assets/styles/bootstrap.custom.css';
 
 import { RouterProvider } from 'react-router';
 import { router } from './routes/Routes.jsx';
+import { Provider } from 'react-redux';
+import store from './store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,30 +1,45 @@
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useGetProductsQuery } from '../slices/productApiSlice';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const { data: products, isloading, error } = useGetProductsQuery();
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
+  // const [products, setProducts] = useState([]);
 
-    fetchProducts();
-  }, []);
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     const { data } = await axios.get('/api/products');
+  //     setProducts(data);
+  //   };
+
+  //   fetchProducts();
+  // }, []);
 
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isloading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant={'danger'}>
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products?.map((product) => (
+              <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
